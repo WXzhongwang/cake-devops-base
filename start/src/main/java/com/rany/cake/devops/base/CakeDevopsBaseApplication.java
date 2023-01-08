7a859b689e33d29745e7159cd33da96636f2ae2b
zhongshengwang <1528683621@qq.com>
2023-01-08 21:34:12 +0800

    init

---
+++ start/src/main/java/com/rany/cake/devops/base/CakeDevopsBaseApplication.java
@@ -0,0 +1,17 @@
+package com.rany.cake.devops.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Cake devops
+ * @author zhongshengwang
+ */
+@SpringBootApplication
+public class CakeDevopsBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CakeDevopsBaseApplication.class, args);
+    }
+
+}
---
+++ start/src/test/java/com/rany/cake/devops/base/CakeDevopsBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rany.cake.devops.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeDevopsBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
