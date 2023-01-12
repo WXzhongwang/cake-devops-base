af324064d8cecf131e3cdb58c702661e69310ae9
zhongshengwang <1528683621@qq.com>
2023-01-12 23:46:58 +0800

    AppAggregate

---
+++ cake-devops-base-api/src/main/java/com/rany/cake/devops/base/api/enums/DevelopMode.java
@@ -0,0 +1,28 @@
+package com.rany.cake.devops.base.api.enums;
+
+/**
+ * TODO
+ *
+ * @author zhongshengwang
+ * @description TODO
+ * @date 2023/1/12 23:42
+ * @email 18668485565163.com
+ */
+public enum DevelopMode {
+
+    /**
+     * 分支开发模式
+     */
+    BRANCH,
+
+    /**
+     * gitflow模式
+     */
+    FLOW,
+
+    /**
+     * 主干模式(自由模式)
+     */
+    FREEDOM
+
+}
