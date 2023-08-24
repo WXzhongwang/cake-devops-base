4bdd82e3401fe729799ff23cc0f19e9ea75378fc
zhongshengwang <1528683621@qq.com>
2023-08-24 22:23:35 +0800

    DBMS模型设计

---
+++ cake-devops-base-service/src/main/java/com/rany/cake/devops/base/service/plugins/builds/DotNetBuildPlugin.java
@@ -0,0 +1,25 @@
+package com.rany.cake.devops.base.service.plugins.builds;
+
+import com.rany.cake.devops.base.service.context.DeployContext;
+import com.rany.cake.devops.base.service.plugins.BasePlugin;
+
+/**
+ * jenkins构建
+ *
+ * @author zhongshengwang
+ * @description TODO
+ * @date 2023/1/19 18:23
+ * @email 18668485565163.com
+ */
+public class DotNetBuildPlugin extends BasePlugin {
+
+    @Override
+    public boolean init(DeployContext context) {
+        return false;
+    }
+
+    @Override
+    public boolean execute(DeployContext context) {
+        return true;
+    }
+}
