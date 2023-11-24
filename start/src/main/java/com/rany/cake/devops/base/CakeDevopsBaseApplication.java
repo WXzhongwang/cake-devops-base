1679e0e9bc616a9800347c4ce4c1ad6fc6918419
1528683621@qq.com <18668485565@163.com>
2023-11-24 22:12:57 +0800

    feat: redis integration.

--- start/src/main/java/com/rany/cake/devops/base/CakeDevopsBaseApplication.java
+++ start/src/main/java/com/rany/cake/devops/base/CakeDevopsBaseApplication.java
@@ -17,7 +17,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @SpringBootApplication(exclude = {ParamLimitInterceptor.class})
 @MapperScan(basePackages = {"com.rany.cake.devops.base.infra.dao", "com.rany.cake.devops.base.infra.mapper"})
-@ComponentScan({"com.rany.cake.devops.base.domain", "com.rany.cake.devops.base.web", "com.rany.cake.devops.base.service", "com.rany.cake.devops.base.infra"})
+@ComponentScan({"com.rany.cake.devops.base", "com.rany.cake.devops.base.domain", "com.rany.cake.devops.base.web", "com.rany.cake.devops.base.service", "com.rany.cake.devops.base.infra"})
 public class CakeDevopsBaseApplication {
 
     public static void main(String[] args) {
