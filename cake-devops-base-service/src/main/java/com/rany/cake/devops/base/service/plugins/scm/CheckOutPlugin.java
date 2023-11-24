08ef37f3d8b0f05df628fa6be9c4b917d2088992
1528683621@qq.com <18668485565@163.com>
2023-11-24 08:44:49 +0800

    feat: redis integration.

--- cake-devops-base-service/src/main/java/com/rany/cake/devops/base/service/plugins/scm/CheckOutPlugin.java
+++ cake-devops-base-service/src/main/java/com/rany/cake/devops/base/service/plugins/scm/CheckOutPlugin.java
@@ -6,6 +6,8 @@ import com.rany.cake.devops.base.service.code.BaseCodeService;
 import com.rany.cake.devops.base.service.code.CodeFactory;
 import com.rany.cake.devops.base.service.context.DeployContext;
 import com.rany.cake.devops.base.service.plugins.BasePlugin;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -18,6 +20,7 @@ import javax.annotation.Resource;
  * @date 2023/1/20 20:30
  * @email 18668485565163.com
  */
+@Slf4j
 @Component
 public class CheckOutPlugin extends BasePlugin {
     @Override
@@ -31,6 +34,10 @@ public class CheckOutPlugin extends BasePlugin {
     @Override
     public boolean execute(DeployContext context) {
         CodeRepository app = context.getApp().getCodeRepository();
+        // 默认以分支为基准分支
+        String ref = StringUtils.isNotEmpty(context.getRelease().getReleaseBranch()) ?
+                context.getRelease().getReleaseBranch() : context.getRelease().getReleaseCommitId();
+        log.info("Current code repo:{}, default branch:{}", app.getRepo(), app.getDefaultBranch());
         BaseCodeService codeService = codeFactory.build(CodePlatformEnum.GITLAB, "127.0.0.0:12345", "tokexxxx");
         return true;
     }
