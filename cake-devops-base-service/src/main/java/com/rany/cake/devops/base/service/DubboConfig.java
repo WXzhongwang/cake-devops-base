c8831648fc834c3e3805ac3ed63ca5b548568ade
1528683621@qq.com <18668485565@163.com>
2023-12-02 22:40:51 +0800

    feat: delivery

--- cake-devops-base-service/src/main/java/com/rany/cake/devops/base/service/DubboConfig.java
+++ cake-devops-base-service/src/main/java/com/rany/cake/devops/base/service/DubboConfig.java
@@ -17,7 +17,7 @@ import org.springframework.context.annotation.Configuration;
 public class DubboConfig {
 
     // @Reference(check = false)
-    @Reference
+    @Reference(check = false)
     private AccountFacade accountFacade;
 
     @Bean
