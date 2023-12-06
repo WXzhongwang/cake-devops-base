3ea76add60d8e85af4216e1f5928734f0f667bc0
1528683621@qq.com <18668485565@163.com>
2023-12-06 21:57:24 +0800

    feat: 终端相关表设计

---
+++ cake-devops-base-ssh/src/main/java/com/rany/cake/devops/ssh/base/enums/InstanceSessionTypeEnum.java
@@ -0,0 +1,22 @@
+package com.rany.cake.devops.ssh.base.enums;
+
+import lombok.Getter;
+
+
+@Getter
+public enum InstanceSessionTypeEnum {
+
+    /**
+     * 实例会话类型
+     */
+    SERVER("SERVER"),
+    // container
+    CONTAINER_LOG("CONTAINER_LOG"),
+    CONTAINER_TERMINAL("CONTAINER_TERMINAL");
+
+    private final String type;
+
+    InstanceSessionTypeEnum(String type) {
+        this.type = type;
+    }
+}
---
+++ cake-devops-base-ssh/src/main/java/com/rany/cake/devops/ssh/base/enums/MessageState.java
@@ -0,0 +1,36 @@
+package com.rany.cake.devops.ssh.base.enums;
+
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
+public enum MessageState {
+
+    /**
+     * 消息状态
+     */
+    LOGIN("LOGIN", "会话初始建立"),
+    HEARTBEAT("HEARTBEAT", "心跳，保持会话"),
+    COMMAND("COMMAND", "交互命令"),
+    DUPLICATE_SESSION("DUPLICATE_SESSION", "复制会话"),
+    DUPLICATE_SESSION_IP("DUPLICATE_SESSION_IP", "复制会话"),
+    RESIZE("RESIZE", "改变形体"),
+    CLOSE("CLOSE", "关闭所有Term会话"),
+    LOGOUT("LOGOUT", "关闭Term会话"),
+    BATCH_COMMAND("BATCH_COMMAND", "批量命令"),
+
+    PLAY("PLAY", "播放"),
+    ;
+    private final String state;
+    private final String desc;
+
+    MessageState(String code, String desc) {
+        this.state = code;
+        this.desc = desc;
+    }
+
+    public String getDescByState(String state) {
+        return Arrays.stream(MessageState.values()).filter(typeEnum -> typeEnum.getState().equals(state)).findFirst().map(MessageState::getDesc).orElse("undefined");
+    }
+}
