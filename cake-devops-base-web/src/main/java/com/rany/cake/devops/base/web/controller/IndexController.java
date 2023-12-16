53034c1009f737a7cac6242cf3c1067a1b20ce5f
1528683621@qq.com <18668485565@163.com>
2023-12-16 23:42:15 +0800

    feat: user login

--- cake-devops-base-web/src/main/java/com/rany/cake/devops/base/web/controller/IndexController.java
+++ cake-devops-base-web/src/main/java/com/rany/cake/devops/base/web/controller/IndexController.java
@@ -6,10 +6,12 @@ import com.rany.cake.dingtalk.sdk.utils.SsoTokenLoginHelper;
 import com.rany.cake.dingtalk.sdk.utils.SsoUtil;
 import com.rany.cake.dingtalk.starter.annotation.CurrentUser;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,12 +22,12 @@ import javax.servlet.http.HttpServletResponse;
  */
 @Controller
 public class IndexController {
-    @RequestMapping(value = "")
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
     public String index() {
         return "index";
     }
 
-    @RequestMapping(value = "/**/{path:[^\\.]*}")
+    @RequestMapping(value = "/**/{path:[^\\.]*}", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
     public String home() {
         return "index";
     }
--- cake-devops-base-web/src/main/java/com/rany/cake/devops/base/web/controller/UserController.java
+++ cake-devops-base-web/src/main/java/com/rany/cake/devops/base/web/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.rany.cake.devops.base.web.controller;
 
+import com.cake.framework.common.response.PojoResult;
 import com.rany.cake.dingtalk.sdk.beans.SsoUser;
 import com.rany.cake.dingtalk.sdk.configuration.SsoConstants;
 import com.rany.cake.dingtalk.sdk.utils.SsoTokenLoginHelper;
@@ -7,19 +8,21 @@ import com.rany.cake.dingtalk.sdk.utils.SsoUtil;
 import com.rany.cake.dingtalk.starter.annotation.CurrentUser;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
-@RestController("/api/devops/")
+@RestController
+@RequestMapping("/api/devops/user")
 public class UserController {
 
     @GetMapping("/getUser")
-    public SsoUser getUser(HttpServletRequest request) {
+    public PojoResult<SsoUser> getUser(HttpServletRequest request) {
         String token = request.getHeader(SsoConstants.TOKEN_AUTH_HEADER);
         if (StringUtils.isNotEmpty(token)) {
-            return SsoTokenLoginHelper.getStorageUser(token);
+            return PojoResult.succeed(SsoTokenLoginHelper.getStorageUser(token));
         }
-        return SsoUtil.getCurrentUser(request);
+        return PojoResult.succeed(SsoUtil.getCurrentUser(request));
     }
 }
