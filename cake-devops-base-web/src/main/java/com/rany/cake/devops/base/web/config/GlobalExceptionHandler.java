d690d7fc25ae10084889a3c081e4b83da2136dac
1528683621@qq.com <18668485565@163.com>
2023-12-18 08:33:04 +0800

    feat: GlobalExceptionHandler

--- cake-devops-base-web/src/main/java/com/rany/cake/devops/base/web/config/GlobalExceptionHandler.java
+++ cake-devops-base-web/src/main/java/com/rany/cake/devops/base/web/config/GlobalExceptionHandler.java
@@ -5,7 +5,6 @@ import com.cake.framework.common.response.PojoResult;
 import com.rany.uic.common.exception.BusinessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
