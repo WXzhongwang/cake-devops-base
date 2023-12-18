9afeb4e0b2d8ac1037d4645d4c3cc8f709379ffd
1528683621@qq.com <18668485565@163.com>
2023-12-18 08:32:51 +0800

    feat: GlobalExceptionHandler

---
+++ cake-devops-base-web/src/main/java/com/rany/cake/devops/base/web/config/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.rany.cake.devops.base.web.config;
+
+
+import com.cake.framework.common.response.PojoResult;
+import com.rany.uic.common.exception.BusinessException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value = { BusinessException.class })
+    public PojoResult<Object> handleBizException(BusinessException ex) {
+        log.error("An biz exception occurred: ", ex);
+        return new PojoResult<>(false, ex.getCode(), ex.getMessage());
+    }
+    @ExceptionHandler(value = { Exception.class })
+    public PojoResult<Object> handleException(Exception ex) {
+        log.error("An system exception occurred: ", ex);
+        return new PojoResult<>(false, String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()), ex.getMessage());
+    }
+}
