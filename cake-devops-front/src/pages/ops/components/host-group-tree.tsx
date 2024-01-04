771e8d055c8fb33497333a80c26c7b28279a3502
1528683621@qq.com <18668485565@163.com>
2024-01-04 23:15:05 +0800

    host-list

--- cake-devops-front/src/pages/ops/components/host-group-tree.tsx
+++ cake-devops-front/src/pages/ops/components/host-group-tree.tsx
@@ -1,25 +1,49 @@
 // src/components/HostGroupTree.tsx
 import React from "react";
 import { Tree } from "antd";
-import { HostGroupTreeDTO } from "@/models/host";
+import { HostGroupModel } from "@/models/host";
 
 interface HostGroupTreeProps {
-  data: HostGroupTreeDTO[];
+  data: HostGroupModel[];
   onGroupSelect: (groupId: string) => void;
 }
+
+const convertDataToTree = (data: HostGroupModel[]): any => {
+  return data.map((item) => {
+    const treeNode: any = {
+      key: item.hostGroupId,
+      title: item.name,
+    };
+
+    if (item.children && item.children.length > 0) {
+      treeNode.children = convertDataToTree(item.children);
+    }
+
+    return treeNode;
+  });
+};
 
 const HostGroupTree: React.FC<HostGroupTreeProps> = ({
   data,
   onGroupSelect,
 }) => {
-  const handleTreeSelect = (selectedKeys: string[]) => {
+  // const handleTreeSelect = (selectedKeys: string[]) => {
+  //   if (selectedKeys.length > 0) {
+  //     // 传递选择的分组信息给父组件
+  //     onGroupSelect(selectedKeys[0]);
+  //   }
+  // };
+
+  const handleTreeSelect = (selectedKeys: React.Key[], info: any) => {
     if (selectedKeys.length > 0) {
       // 传递选择的分组信息给父组件
-      onGroupSelect(selectedKeys[0]);
+      onGroupSelect(selectedKeys[0] as string);
     }
   };
 
-  return <Tree treeData={data} onSelect={handleTreeSelect} />;
+  const treeData = convertDataToTree(data);
+
+  return <Tree treeData={treeData} onSelect={handleTreeSelect} />;
 };
 
 export default HostGroupTree;
