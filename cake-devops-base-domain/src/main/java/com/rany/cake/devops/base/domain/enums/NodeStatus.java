452409beae09818c905814f55e64b97bd136da07
zhongshengwang <1528683621@qq.com>
2024-01-20 10:48:44 +0800

    关闭，发布，查看审批单的能力

---
+++ cake-devops-base-domain/src/main/java/com/rany/cake/devops/base/domain/enums/NodeStatus.java
@@ -0,0 +1,26 @@
+package com.rany.cake.devops.base.domain.enums;
+
+public enum ReleaseStatus {
+    /**
+     * 审批中
+     */
+    AWAIT_APPROVAL,
+    /**
+     * 待发布
+     */
+    READY,
+
+    /**
+     * 发布中
+     */
+    PENDING,
+
+    /**
+     * 已发布
+     */
+    FINISHED,
+    /**
+     * 发布失败
+     */
+    FAILED,
+}
---
+++ cake-devops-base-service/src/main/java/com/rany/cake/devops/base/service/plugins/annotation/PluginName.java
@@ -0,0 +1,2 @@
+package com.rany.cake.devops.base.service.plugins.annotation;public class PluginName {
+}
