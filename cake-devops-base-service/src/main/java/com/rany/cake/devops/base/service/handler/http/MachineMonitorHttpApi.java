8914c69a05ae283afb0a18493ef576f6adf4dc27
zhongshengwang <1528683621@qq.com>
2024-03-25 21:25:22 +0800

    feat: 系统环境变量

--- cake-devops-base-service/src/main/java/com/rany/cake/devops/base/service/handler/http/MachineMonitorHttpApi.java
+++ cake-devops-base-service/src/main/java/com/rany/cake/devops/base/service/handler/http/MachineMonitorHttpApi.java
@@ -18,57 +18,57 @@ public enum MachineMonitorHttpApi implements HttpApiDefined {
     /**
      * 端点 ping
      */
-    ENDPOINT_PING("/orion/machine-monitor-agent/api/endpoint/ping", HttpMethod.GET),
+    ENDPOINT_PING("/cake/machine-monitor-agent/api/endpoint/ping", HttpMethod.GET),
 
     /**
      * 端点 version
      */
-    ENDPOINT_VERSION("/orion/machine-monitor-agent/api/endpoint/version", HttpMethod.GET),
+    ENDPOINT_VERSION("/cake/machine-monitor-agent/api/endpoint/version", HttpMethod.GET),
 
     /**
      * 端点 sync
      */
-    ENDPOINT_SYNC("/orion/machine-monitor-agent/api/endpoint/sync", HttpMethod.POST),
+    ENDPOINT_SYNC("/cake/machine-monitor-agent/api/endpoint/sync", HttpMethod.POST),
 
     /**
      * 指标 获取机器基本指标
      */
-    METRICS_BASE("/orion/machine-monitor-agent/api/metrics/base", HttpMethod.GET),
+    METRICS_BASE("/cake/machine-monitor-agent/api/metrics/base", HttpMethod.GET),
 
     /**
      * 指标 获取系统负载
      */
-    METRICS_SYSTEM_LOAD("/orion/machine-monitor-agent/api/metrics/system-load", HttpMethod.GET),
+    METRICS_SYSTEM_LOAD("/cake/machine-monitor-agent/api/metrics/system-load", HttpMethod.GET),
 
     /**
      * 指标 获取硬盘名称
      */
-    METRICS_DISK_NAME("/orion/machine-monitor-agent/api/metrics/disk-name", HttpMethod.GET),
+    METRICS_DISK_NAME("/cake/machine-monitor-agent/api/metrics/disk-name", HttpMethod.GET),
 
     /**
      * 指标 获取 top 进程
      */
-    METRICS_TOP_PROCESSES("/orion/machine-monitor-agent/api/metrics/top-processes", HttpMethod.GET),
+    METRICS_TOP_PROCESSES("/cake/machine-monitor-agent/api/metrics/top-processes", HttpMethod.GET),
 
     /**
      * 监控 获取cpu数据
      */
-    MONITOR_CPU("/orion/machine-monitor-agent/api/monitor-statistic/cpu", HttpMethod.POST),
+    MONITOR_CPU("/cake/machine-monitor-agent/api/monitor-statistic/cpu", HttpMethod.POST),
 
     /**
      * 监控 获取内存数据
      */
-    MONITOR_MEMORY("/orion/machine-monitor-agent/api/monitor-statistic/memory", HttpMethod.POST),
+    MONITOR_MEMORY("/cake/machine-monitor-agent/api/monitor-statistic/memory", HttpMethod.POST),
 
     /**
      * 监控 获取网络数据
      */
-    MONITOR_NET("/orion/machine-monitor-agent/api/monitor-statistic/net", HttpMethod.POST),
+    MONITOR_NET("/cake/machine-monitor-agent/api/monitor-statistic/net", HttpMethod.POST),
 
     /**
      * 监控 获取磁盘数据
      */
-    MONITOR_DISK("/orion/machine-monitor-agent/api/monitor-statistic/disk", HttpMethod.POST),
+    MONITOR_DISK("/cake/machine-monitor-agent/api/monitor-statistic/disk", HttpMethod.POST),
 
     ;
 
