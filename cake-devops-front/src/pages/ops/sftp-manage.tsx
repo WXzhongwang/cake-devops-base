9698309c02193ab67f99d7b704cbfa5048fe803d
zhongshengwang <1528683621@qq.com>
2024-06-25 21:55:09 +0800

    sftp

--- cake-devops-front/src/pages/ops/sftp-manage.tsx
+++ cake-devops-front/src/pages/ops/sftp-manage.tsx
@@ -42,6 +42,7 @@ import {
   FolderOutlined,
   InboxOutlined,
   IssuesCloseOutlined,
+  LoadingOutlined,
   PauseOutlined,
   PlayCircleOutlined,
   PlusOutlined,
@@ -53,6 +54,9 @@ import {
   UploadOutlined,
 } from "@ant-design/icons";
 import { connect, Dispatch, useParams } from "umi";
+
+import type { UploadChangeParam } from "antd/es/upload";
+import type { RcFile, UploadFile, UploadProps } from "antd/es/upload/interface";
 
 const { Option } = Select;
 
@@ -70,6 +74,9 @@ const SftpManagementPage: React.FC<SftpManageProps> = ({
   files,
   transferList,
 }) => {
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState<string>();
+
   const { id } = useParams();
   const [directoryTreeData, setDirectoryTreeData] = useState<any[]>([]);
   const [selectedDirectoryKey, setSelectedDirectoryKey] = useState<
@@ -578,6 +585,48 @@ const SftpManagementPage: React.FC<SftpManageProps> = ({
         },
       });
     });
+
+  const getBase64 = (img: RcFile, callback: (url: string) => void) => {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => callback(reader.result as string));
+    reader.readAsDataURL(img);
+  };
+
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  );
+
+  const beforeUpload = (file: RcFile) => {
+    //   const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+    //   if (!isJpgOrPng) {
+    //     message.error("You can only upload JPG/PNG file!");
+    //   }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+      message.error("Image must smaller than 2MB!");
+    }
+    return isLt2M;
+  };
+
+  const handleChange: UploadProps["onChange"] = (
+    info: UploadChangeParam<UploadFile>
+  ) => {
+    if (info.file.status === "uploading") {
+      setLoading(true);
+      return;
+    }
+    if (info.file.status === "done") {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj as RcFile, (url) => {
+        setLoading(false);
+        setImageUrl(url);
+      });
+    }
+  };
+
   return (
     <PageContainer title="SFTP">
       <Card>
@@ -812,7 +861,7 @@ const SftpManagementPage: React.FC<SftpManageProps> = ({
                           }}
                           align="center"
                         >
-                          <Upload.Dragger
+                          {/* <Upload.Dragger
                             multiple
                             name="file"
                             action={"/upload/api"}
@@ -826,7 +875,28 @@ const SftpManagementPage: React.FC<SftpManageProps> = ({
                             >
                               单击或拖动文件到此区域进行上传
                             </p>
-                          </Upload.Dragger>
+                          </Upload.Dragger> */}
+
+                          <Upload
+                            name="avatar"
+                            listType="picture-card"
+                            className="avatar-uploader"
+                            showUploadList={false}
+                            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                            beforeUpload={beforeUpload}
+                            onChange={handleChange}
+                          >
+                            {imageUrl ? (
+                              <img
+                                src={imageUrl}
+                                alt="avatar"
+                                style={{ width: "100%" }}
+                              />
+                            ) : (
+                              uploadButton
+                            )}
+                          </Upload>
+
                           <Space direction="vertical">
                             <Button type="primary">上传</Button>
                             <Button>清空</Button>
