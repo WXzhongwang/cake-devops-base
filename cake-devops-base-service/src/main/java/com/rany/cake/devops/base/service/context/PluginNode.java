10e105489f90da5ce0e4c5ee3b27dfd33c6718cc
zhongshengwang <1528683621@qq.com>
2024-07-17 23:57:28 +0800

    feat: 全流程结束时标记成功

--- cake-devops-base-service/src/main/java/com/rany/cake/devops/base/service/context/PluginNode.java
+++ cake-devops-base-service/src/main/java/com/rany/cake/devops/base/service/context/PluginNode.java
@@ -58,24 +58,29 @@ public class PluginNode implements Plugin {
 
     @Override
     public boolean execute(DeployContext context) {
-        log.info("{}开始执行", this.name);
+        log.info("{}，开始执行", this.name);
         context.increment();
         DeployContext.Node node = context.getProgress().getSteps().get(context.current());
         node.setStartDate(new Date());
         context.setCurrentPluginName(this.name);
         context.getPluginNames().add(this.name);
         if (!plugin.execute(context) && stopWhenFailure()) {
-            log.info("{}执行结束", this.name);
+            log.info("{}，执行结束", this.name);
             node.setEndDate(new Date());
             node.setDescription(NodeStatus.EXECUTED.name());
             observer.updateProgress(context);
             return false;
         }
-        log.info("{}执行结束", this.name);
+        log.info("{}，执行结束", this.name);
         node.setEndDate(new Date());
         node.setDescription(NodeStatus.EXECUTED.name());
         observer.updateProgress(context);
-        return next != null && next.execute(context);
+        if (next == null) {
+            log.info("{}，全流程执行结束", this.name);
+            observer.updateProgress(context);
+            return true;
+        }
+        return next.execute(context);
     }
 
     @Override
