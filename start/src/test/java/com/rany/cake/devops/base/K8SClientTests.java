26e3d52b2a647a1d7c8d8fbb85bbe1d5c0be2f96
zhongshengwang <1528683621@qq.com>
2024-07-29 21:18:50 +0800

    feat: health check

--- start/src/test/java/com/rany/cake/devops/base/K8SClientTests.java
+++ start/src/test/java/com/rany/cake/devops/base/K8SClientTests.java
@@ -92,7 +92,7 @@ public class K8SClientTests extends BaseTests {
         context.setDeploymentName(app.getAppName().getName());
         context.setServiceName(app.getAppName().getName() + "-svc");
         context.setServicePort(8080);
-        context.setContainerPort(7001);
+        context.setContainerPort(8300);
         context.setIngressName(app.getAppName().getName());
         context.setDeploymentImage("registry.cn-hangzhou.aliyuncs.com/cake-devops-base/cake-devops-base:R202407292031050013");
         boolean deployment = cloudService.createDeployment(context);
