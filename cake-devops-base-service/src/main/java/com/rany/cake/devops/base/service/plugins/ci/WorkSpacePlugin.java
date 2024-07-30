de77afd9c0866821a2ec8a9dcfc6153174689ba5
zhongshengwang <1528683621@qq.com>
2024-07-30 22:52:22 +0800

    feat: configMap

--- cake-devops-base-service/src/main/java/com/rany/cake/devops/base/service/plugins/ci/WorkSpacePlugin.java
+++ cake-devops-base-service/src/main/java/com/rany/cake/devops/base/service/plugins/ci/WorkSpacePlugin.java
@@ -63,7 +63,7 @@ public class WorkSpacePlugin extends BasePlugin {
                 });
                 RETRYER.call(() -> {
                     if (!JSCHTool.remoteExecute(session, "cd " + workspace + ";" +
-                            " curl -JLO https://github.com/WXzhongwang/cake-devops-base/releases/download/beta-v0.0.2/java-build-source.tar.gz; " +
+                            " curl -JLO https://github.com/WXzhongwang/cake-devops-base/releases/download/beta-v0.0.3/java-build-source.tar.gz; " +
                             " tar -zxvf java-build-source.tar.gz;" +
                             " chmod +x *.sh;")) {
                         log.error("脚本下载失败");
