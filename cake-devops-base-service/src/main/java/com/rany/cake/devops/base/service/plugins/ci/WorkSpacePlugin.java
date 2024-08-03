be9215829ded1b2f47d9d7102b36f12ac582f746
zhongshengwang <1528683621@qq.com>
2024-08-03 15:44:42 +0800

    feat: 部署脚本

--- cake-devops-base-service/src/main/java/com/rany/cake/devops/base/service/plugins/ci/WorkSpacePlugin.java
+++ cake-devops-base-service/src/main/java/com/rany/cake/devops/base/service/plugins/ci/WorkSpacePlugin.java
@@ -63,7 +63,7 @@ public class WorkSpacePlugin extends BasePlugin {
                 });
                 RETRYER.call(() -> {
                     if (!JSCHTool.remoteExecute(session, "cd " + workspace + ";" +
-                            " curl -JLO https://github.com/WXzhongwang/cake-devops-base/releases/download/beta-v0.0.5/java-build-source.tar.gz; " +
+                            " curl -JLO https://rany-ops.oss-cn-hangzhou.aliyuncs.com/ci/0.0.6/java-build-source.tar.gz; " +
                             " tar -zxvf java-build-source.tar.gz;" +
                             " chmod +x *.sh;")) {
                         log.error("脚本下载失败");
