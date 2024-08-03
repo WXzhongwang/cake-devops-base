d6893698199fe03bb41717efe25fba220dcb5dbe
zhongshengwang <1528683621@qq.com>
2024-08-03 20:49:24 +0800

    feat: 部署脚本

--- appctl.sh
+++ appctl.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-
 # 检查命令行参数
 if [ $# -lt 1 ]; then
-    usage
+    echo "Usage: $0 {start|stop|restart}" >&2
     exit 2 # bad usage
 fi
 
 # 操作
 APP_NAME=$1
-# 操作
 ACTION=$2
 
 echo "${APP_NAME}"
@@ -19,12 +17,17 @@ echo "${ACTION}"
 IN_DOCKER=$(command -v docker &> /dev/null && echo true || echo false)
 
 # 定义日志文件
-LOGFILE="/home/admin/${APP_NAME}/server_std_out.log"
 DEPLOY_LOG="/home/admin/${APP_NAME}/deploy.log"
 
 # 定义 Java 主类和 jar 文件路径
 MAIN_CLASS="com.rany.cake.devops.base.CakeDevopsBaseApplication"
 JAR_FILE="/home/admin/${APP_NAME}/cake-devops-service.jar"
+
+# 检查 JAR 文件是否存在
+if [ ! -f "$JAR_FILE" ]; then
+    echo "JAR file does not exist at path: $JAR_FILE" >&2
+    exit 1
+fi
 
 # 定义 JVM 选项
 JAVA_OPTS=(
@@ -48,43 +51,34 @@ START_CMD="java ${JAVA_OPTS[*]} -Dspring.profiles.active=$SPRING_PROFILES_ACTIVE
 
 # 检查应用是否正在运行
 is_running() {
-    local pipe_status=()
     if [[ $IN_DOCKER == true ]]; then
-        pgrep -f "$MAIN_CLASS"
-        pipe_status=($?)
+        pgrep -f "$MAIN_CLASS" > /dev/null
+        return $?
     else
-        ps aux | grep "[j]ava.*$MAIN_CLASS" | grep -v grep
-        pipe_status=($?)
-    fi
-    check_first_pipe_exit_code "${pipe_status[@]}"
-}
-
-# 检查管道中第一个命令的退出状态
-check_first_pipe_exit_code() {
-    if [[ $1 -ne 0 ]]; then
-        echo "Error in pipeline, first command failed with status $1"
-        exit $1
+        ps aux | grep "[j]ava.*$MAIN_CLASS" | grep -v grep > /dev/null
+        return $?
     fi
 }
 
 # 启动应用
 start_app() {
     echo "Starting application..." | tee -a "$DEPLOY_LOG"
-    if is_running | grep -q .; then
+    if is_running; then
         echo "Application is already running." | tee -a "$DEPLOY_LOG"
     else
-      # 使用nohup来让Java应用在后台运行，并使用tee同时输出到控制台和日志文件
-      nohup $START_CMD | tee -a "$LOGFILE" &
-      echo "Application started." | tee -a "$DEPLOY_LOG"
-      # 输出应用PID到另一个文件，方便后续操作
-      echo $! > /home/admin/${APP_NAME}/app.pid
+        # 使用nohup来让Java应用在后台运行
+        # 注意：这里不再重定向到日志文件，而是直接启动应用
+        nohup $START_CMD &
+        echo "Application started." | tee -a "$DEPLOY_LOG"
+        # 输出应用PID到另一个文件，方便后续操作
+        echo $! > /home/admin/${APP_NAME}/app.pid
     fi
 }
 
 # 停止应用
 stop_app() {
     echo "Stopping application..." | tee -a "$DEPLOY_LOG"
-    if is_running | grep -q .; then
+    if is_running; then
         if [[ $IN_DOCKER == true ]]; then
             pkill -f "$MAIN_CLASS"
         else
@@ -101,7 +95,6 @@ restart_app() {
     stop_app
     start_app
 }
-
 
 # 执行启动命令
 case "$ACTION" in
