622890d6ffd2b45e8cd86b40606c131030e694c5
zhongshengwang <1528683621@qq.com>
2024-08-11 15:08:33 +0800

    feat: ingress

--- cake-devops-base-api/src/main/java/com/rany/cake/devops/base/api/query/alarm/HostAlarmHistoryPageQuery.java
+++ cake-devops-base-api/src/main/java/com/rany/cake/devops/base/api/query/alarm/HostAlarmHistoryPageQuery.java
@@ -1,6 +1,7 @@
 package com.rany.cake.devops.base.api.query.alarm;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+//import com.fasterxml.jackson.annotation.JsonFormat;
+
 import com.rany.cake.devops.base.api.common.base.BasePageQuery;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -13,9 +14,9 @@ public class HostAlarmHistoryPageQuery extends BasePageQuery {
 
     private String hostId;
     private Integer alarmType;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    //    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date startDate;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    //    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date endDate;
     private Double minValue;
     private Double maxValue;
--- cake-devops-base-web/src/main/java/com/rany/cake/devops/base/web/config/SwaggerConfig.java
+++ cake-devops-base-web/src/main/java/com/rany/cake/devops/base/web/config/SwaggerConfig.java
@@ -1,55 +1,55 @@
-package com.rany.cake.devops.base.web.config;
-
-import com.rany.cake.devops.base.util.Const;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-
-/**
- * swagger 配置
- * <p>
- * <a href="http://localhost:8300/doc.html">...</a>
- *
- * @author zhongshengwang
- * @version 1.0.0
- * @since 2022/7/8 10:13
- */
-@Configuration
-//@EnableSwagger2
-//@EnableKnife4j
-@Profile({"dev"})
-public class SwaggerConfig {
-    @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(this.getApiInfo())
-                //.securitySchemes(this.getSecuritySchemes())
-                .enable(true)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.rany.cake.devops.base.web.controller"))
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    /**
-     * 配置 api 信息
-     *
-     * @return api info
-     */
-    private ApiInfo getApiInfo() {
-        return new ApiInfoBuilder()
-                .title("Cake Devops restful API")
-                .contact(new Contact(Const.AUTHOR, Const.AUTHOR_GITHUB, Const.AUTHOR_EMAIL))
-                .version(Const.VERSION)
-                .description("Cake Devops api管理")
-                .build();
-    }
-
-}
+//package com.rany.cake.devops.base.web.config;
+//
+//import com.rany.cake.devops.base.util.Const;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.service.Contact;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//
+///**
+// * swagger 配置
+// * <p>
+// * <a href="http://localhost:8300/doc.html">...</a>
+// *
+// * @author zhongshengwang
+// * @version 1.0.0
+// * @since 2022/7/8 10:13
+// */
+//@Configuration
+////@EnableSwagger2
+////@EnableKnife4j
+//@Profile({"dev"})
+//public class SwaggerConfig {
+//    @Bean
+//    public Docket createRestApi() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(this.getApiInfo())
+//                //.securitySchemes(this.getSecuritySchemes())
+//                .enable(true)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.rany.cake.devops.base.web.controller"))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//
+//    /**
+//     * 配置 api 信息
+//     *
+//     * @return api info
+//     */
+//    private ApiInfo getApiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("Cake Devops restful API")
+//                .contact(new Contact(Const.AUTHOR, Const.AUTHOR_GITHUB, Const.AUTHOR_EMAIL))
+//                .version(Const.VERSION)
+//                .description("Cake Devops api管理")
+//                .build();
+//    }
+//
+//}
