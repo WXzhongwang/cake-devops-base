389245bbefb326f156b7fa40d701e50279e03c25
zhongshengwang <1528683621@qq.com>
2024-11-03 18:45:15 +0800

    feat: sql

--- sql/devops-2024_07_30.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE cake_devops.app_env ADD config_map varchar(2000) NULL;
-ALTER TABLE cake_devops.app_env ADD ingress_name varchar(256) NULL;
-ALTER TABLE cake_devops.app_env ADD service_name varchar(256) NULL;
-ALTER TABLE cake_devops.app_env ADD deployment_name varchar(256) NULL;
-ALTER TABLE cake_devops.app_env ADD custom_build_script varchar(256) NULL;
--- sql/env_vars.sql
+++
@@ -1,3 +0,0 @@
-alter table app_env
-    add env_vars varchar(2000) null after config_map;
-
