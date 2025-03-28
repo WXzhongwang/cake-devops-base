e5bc6c588a060cad5be5244b31b0cc283cb11754
zhongshengwang <1528683621@qq.com>
2025-03-28 10:07:13 +0800

    feat: dockerfile

--- APP-META/appctl.sh
+++ APP-META/appctl.sh
@@ -1,55 +1,49 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 # 检查命令行参数
 if [ $# -lt 2 ] || [ $# -gt 3 ]; then
-    echo "Usage: $0 {start|stop|restart} <app_name> [spring_profiles_active]" >&2
-    exit 2 # bad usage
+    echo "Usage: $0 <APP_NAME> {start|stop|restart} [spring_profiles_active]" >&2
+    exit 2 # 参数错误
 fi
 
-# 操作
-ACTION=$1
-APP_NAME=$2
+# 操作参数
+APP_NAME=$1
+ACTION=$2
 SPRING_PROFILES_ACTIVE=${3:-dev}
 
 echo "APP_NAME: ${APP_NAME}"
 echo "ACTION: ${ACTION}"
 echo "Spring Profiles Active: ${SPRING_PROFILES_ACTIVE}"
 
-# 定义 PID 文件路径
-PID_FILE="/home/admin/${APP_NAME}/app.pid"
-
-# 确保 PID 文件目录存在
-mkdir -p "$(dirname "$PID_FILE")"
-
-# 定义 Java 主类和 jar 文件路径
-# MAIN_CLASS="com.rany.cake.devops.base.CakeDevopsBaseApplication"
+# 定义路径
 JAR_FILE="/home/admin/${APP_NAME}/cake-devops-service.jar"
+MAIN_CLASS="com.rany.cake.devops.base.CakeDevopsBaseApplication"
 
 # 检查 JAR 文件是否存在
 if [ ! -f "$JAR_FILE" ]; then
-    echo "JAR file does not exist at path: $JAR_FILE" >&2
+    echo "ERROR: JAR file does not exist at path: $JAR_FILE" >&2
     exit 1
 fi
 
 # 定义 JVM 选项
-JAVA_OPTS="-Xms512m -Xmx1024m -Xloggc:/home/admin/${APP_NAME}/gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+PrintGCCause -XX:+PrintGCApplicationStoppedTime -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=100M"
+JAVA_OPTS=(
+    "-XX:+UseContainerSupport"
+    "-XX:InitialRAMPercentage=50.0"
+    "-XX:MaxRAMPercentage=75.0"
+    "-Xloggc:/home/admin/gc.log"
+    "-XX:+PrintGCDetails"
+    "-XX:+PrintGCDateStamps"
+)
 
 # 定义启动命令
-START_CMD="$JAVA_HOME/bin/java $JAVA_OPTS -Dspring.profiles.active=$SPRING_PROFILES_ACTIVE -jar $JAR_FILE"
+START_CMD="java ${JAVA_OPTS[*]} -Dspring.profiles.active=$SPRING_PROFILES_ACTIVE -jar $JAR_FILE"
 
 # 检查应用是否正在运行
 is_running() {
-    if [ -f "$PID_FILE" ]; then
-        pid=$(cat "$PID_FILE")
-        if ps -p "$pid" > /dev/null; then
-            return 0
-        else
-            rm -f "$PID_FILE"
-        fi
-    fi
-    return 1
+    pgrep -f "$MAIN_CLASS" > /dev/null
+    return $?
 }
 
 # 启动应用
@@ -58,10 +52,7 @@ start_app() {
     if is_running; then
         echo "Application is already running."
     else
-        echo "Starting application, exec java cmd..."
-        nohup $START_CMD > /dev/stdout 2>&1 &
-        echo $! > "$PID_FILE"
-        echo "Application started with PID $(cat $PID_FILE)."
+        exec $START_CMD
     fi
 }
 
@@ -69,9 +60,7 @@ start_app() {
 stop_app() {
     echo "Stopping application..."
     if is_running; then
-        # shellcheck disable=SC2046
-        kill $(cat "$PID_FILE")
-        rm -f "$PID_FILE"
+        pkill -f "$MAIN_CLASS"
         echo "Application stopped."
     else
         echo "Application is not running."
@@ -84,7 +73,7 @@ restart_app() {
     start_app
 }
 
-# 执行启动命令
+# 执行操作
 case "$ACTION" in
     start)
         start_app
@@ -96,9 +85,7 @@ case "$ACTION" in
         restart_app
         ;;
     *)
-        echo "Usage: $0 {start|stop|restart} <app_name> [spring_profiles_active]" >&2
+        echo "Usage: $0 <APP_NAME> {start|stop|restart} [spring_profiles_active]" >&2
         exit 1
         ;;
 esac
-
-exit 0
